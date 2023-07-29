dc1cdec9fa052fc40f47e07c3e7772f882fe98a6
Olga-Olga <olga_l@ukr.net>
2023-07-29 23:54:49 +0300

    propType

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -7,7 +7,7 @@ export const Profile = ({ username, tag, location, avatar, stats }) => {
       <div className={css.description}>
         <img src={avatar} alt="User avatar" className={css.avatar} />
         <p className={css.name}>{username}</p>
-        <p className={css.tag}>{tag}</p>
+        <p className={css.tag}>@{tag}</p>
         <p className={css.location}>{location}</p>
       </div>
 
@@ -34,5 +34,9 @@ Profile.propTypes = {
   tag: propTypes.string,
   location: propTypes.string,
   avatar: propTypes.string,
-  stats: propTypes.object,
+  stats: propTypes.shape({
+    followers: propTypes.number.isRequired,
+    views: propTypes.number.isRequired,
+    likes: propTypes.number.isRequired,
+  }),
 };
